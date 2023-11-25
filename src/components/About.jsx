e6f14b2272e4c9d067b93f1721b4645919ff443d
bhargav02550 <21mh1a0584@acoe.edu.in>
2023-11-25 18:32:24 +0530

    Commit-2

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,8 +2,18 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div className="flex justify-center h-screen">
-      <h1 className="sm:text-5xl text-3xl">About Me</h1>
+    <div className=" m-auto flex flex-col items-center mt-8 h-screen md:pl-20">
+      <h1 className="sm:text-5xl text-3xl mb-4">About Me</h1>
+      <div className='bg-transparent flex flex-col md:flex-row'>
+        <div className='p-4 md:max-w-[50%] rounded-2xl border-2 border-black'>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+        <div className=' bg-black p-4 md:ml-20 mt-5'>
+          <h1 className=' text-white'>Hello</h1>
+        </div>
+      </div>
     </div>
   );
 };
