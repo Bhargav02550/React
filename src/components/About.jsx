c6017037b4bd491478bf24cb3032558334424b79
bhargav02550 <21mh1a0584@acoe.edu.in>
2023-11-25 18:40:09 +0530

    3

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,8 +4,8 @@ const About = () => {
   return (
     <div className=" m-auto flex flex-col items-center mt-8 h-screen md:pl-20">
       <h1 className="sm:text-5xl text-3xl mb-4">About Me</h1>
-      <div className='bg-transparent flex flex-col md:flex-row'>
-        <div className='p-4 md:max-w-[50%] rounded-2xl border-2 border-black'>
+      <div className='p-4 bg-transparent flex flex-col md:flex-row'>
+      <div className='p-4 md:max-w-[50%] rounded-2xl border-2 border-black hover:bg-black hover:text-white transition ease-in duration-300'>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </p>
