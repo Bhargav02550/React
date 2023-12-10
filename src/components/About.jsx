8e9057cfe807cb632d24784e07d1b8735a79b87e
bhargav02550 <21mh1a0584@acoe.edu.in>
2023-12-10 10:49:53 +0530

    Box-shadow

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,8 +6,8 @@ const About = () => {
     <div className="font-pop m-auto flex flex-col items-center mt-8 h-screen md:pl-20">
       <h1 className="sm:text-5xl text-3xl mb-4">About Me</h1>
       <div className='p-4 bg-transparent flex flex-col md:flex-row'>
-      <div className=' items-center p-4 md:max-w-[50%] rounded-2xl border-2 border-black hover:bg-lime-400 transition ease-in duration-300 md:h-auto group'>
-        <h1 className=' font-sqr text-[80px] sm:text-10 text-lime-400 transition ease-in duration-300 group-hover:text-white'>BHARGAV</h1>
+      <div className=' shadow-thick md:shadow-none items-center p-4 md:max-w-[50%] rounded-2xl border-2 bg-lime-400 md:bg-white border-black hover:md:bg-lime-400 transition ease-in duration-300 md:h-auto group md:hover:shadow-thick '>
+        <h1 className='font-sqr text-[80px] sm:text-10 text-black md:text-lime-400 transition ease-in duration-300 group-hover:md:text-black'>BHARGAV</h1>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </p>
